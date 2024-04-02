9af19021f710777d128c22bc197cbb7deb1c9beb
HadesZ <bruce_zxy@163.com>
2024-04-02 22:27:34 +0800

    feat: 🎸 init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/*
+ * @Author: HadesZ
+ * @Date: 2024-04-02 21:50:12
+ * @LastEditors: HadesZ
+ * @LastEditTime: 2024-04-02 22:20:43
+ * @Description: 
+ */
+
+console.log('Hello, world!!');
